06c855971d1b90b4ef7624d36d5d5a7a4e6a0e1a
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-10-25 14:16:14 +0530

    Reading data from csv file

--- artapp/models.py
+++ artapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Information(models.Model):
+    date = models.DateField()
+    Arrival = models.IntegerField()
+    Departures = models.IntegerField()
+
+# 2007 M 02
